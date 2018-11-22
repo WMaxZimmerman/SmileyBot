50e952d30f54544b9c7cd90b3ba29049e0c596de
WMaxZimmerman <william.m.zimmerman@gmail.com>
2018-11-21 23:39:39 -0600

    Fixed Ball Prediction to scale by time span and not overall delta

--- SmileyBot/SmileyBot.ApplicationCore/Services/BallPredictionService.cs
+++ SmileyBot/SmileyBot.ApplicationCore/Services/BallPredictionService.cs
@@ -18,11 +18,11 @@ namespace SmileyBot.ApplicationCore.Services
 		}
 	    };
 
-	    var timeSpan = .1;
+	    var timeSpan = .1f;
 	    for (var i = 1; i < 10 ; i++)
 	    {
 		var lastPrediction = predictions[i-1];
-		var newPrediction = GetPrediction(lastPrediction, field, game, (float)(i * timeSpan));
+		var newPrediction = GetPrediction(lastPrediction, field, game, timeSpan);
 		predictions.Add(newPrediction);
 	    }
 
@@ -33,16 +33,21 @@ namespace SmileyBot.ApplicationCore.Services
 	private static LocationPrediction GetPrediction(LocationPrediction ball, FieldService field, GameWrapper game, float deltaTime)
 	{
 	    var prediction = new LocationPrediction();
-	    prediction.DeltaTime = deltaTime;
+	    prediction.DeltaTime = ball.DeltaTime + deltaTime;
 	    prediction.GameTime = game.TimeElapsed + deltaTime;
 	    prediction.Location = new Vec3(ball.Location.X, ball.Location.Y, ball.Location.Z);
 	    prediction.Velocity = new Vec3 (ball.Velocity.X, ball.Velocity.Y, ball.Velocity.Z);
 
+	    var friction = prediction.Location.Z <= GameValuesService.BallRadius ? .6f : 1;
+
 	    // Apply forces
-	    prediction.Location.X += (ball.Velocity.X * deltaTime);
-	    prediction.Location.Y += (ball.Velocity.Y * deltaTime);
+	    prediction.Location.X += (ball.Velocity.X * deltaTime) * friction;
+	    prediction.Location.Y += (ball.Velocity.Y * deltaTime) * friction;
 	    prediction.Location.Z += (ball.Velocity.Z * deltaTime) - (GameValuesService.Gravity * deltaTime);
-	    if (prediction.Location.Z < GameValuesService.BallRadius) prediction.Location.Z = GameValuesService.BallRadius;
+	    if (prediction.Location.Z < GameValuesService.BallRadius)
+	    {
+		prediction.Location.Z = GameValuesService.BallRadius;
+	    }
 	    
 	    return prediction;
 	}
